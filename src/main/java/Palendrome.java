0a39ae971a1a8d294366b7b3b1a45ef89132b7ef
Renu Jaishankar <Renu.Jaishankar@yahoo.com>
2021-05-24 17:37:16 -0400

    comment added

--- src/main/java/Palendrome.java
+++ src/main/java/Palendrome.java
@@ -3,6 +3,10 @@ import java.util.*;
 
 public class Palendrome {
     public static void main(String[] args){
+        //A palindrome is a word, phrase, number, or other sequence of characters which
+        // reads the same backward or forward.
+
+
         Scanner sc=new Scanner(System.in);
         System.out.println("Please enter the string to check if it is a Palendrome");
         String A=sc.next();
