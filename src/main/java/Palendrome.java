400f2c5b96af0c46892ed7d3b39cdd20bb4a50fc
Renu Jaishankar <Renu.Jaishankar@yahoo.com>
2021-05-24 17:51:42 -0400

    third commit

--- src/main/java/Palendrome.java
+++ src/main/java/Palendrome.java
@@ -5,6 +5,7 @@ public class Palendrome {
     public static void main(String[] args){
         //A palindrome is a word, phrase, number, or other sequence of characters which
         // reads the same backward or forward.
+        //Given a string , print Yes if it is a palindrome, print No otherwise.
 
 
         Scanner sc=new Scanner(System.in);
